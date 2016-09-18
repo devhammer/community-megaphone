131a593de233c5644e31ba393ae218d832110e67
G. Andrew Duthie <devhammer@live.com>
2016-09-18 15:40:08 -0400

    Tweaks to add event page, event list

--- src/add-event.ts
+++ src/add-event.ts
@@ -2,26 +2,26 @@
 
 export class Welcome {
   heading = 'Add an Event';
-  firstName = 'John';
-  lastName = 'Doe';
-  previousValue = this.fullName;
+  title = 'Event Title';
+  description = 'This is my description.';
+  previousTitle = this.title;
 
   //Getters can't be directly observed, so they must be dirty checked.
   //However, if you tell Aurelia the dependencies, it no longer needs to dirty check the property.
   //To optimize by declaring the properties that this getter is computed from, uncomment the line below
   //as well as the corresponding import above.
   //@computedFrom('firstName', 'lastName')
-  get fullName() {
-    return `${this.firstName} ${this.lastName}`;
+  get currentTitle() {
+    return `${this.title}`;
   }
 
   submit() {
-    this.previousValue = this.fullName;
-    alert(`Welcome, ${this.fullName}!`);
+    this.previousTitle = this.title;
+    alert(`You submitted, ${this.title}!`);
   }
 
   canDeactivate() {
-    if (this.fullName !== this.previousValue) {
+    if (this.title !== this.previousTitle) {
       return confirm('Are you sure you want to leave?');
     }
   }
